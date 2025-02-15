5a49bebafc10ccea19d049219290e57930390ff4
Jonathan Durrance <101822482+ANOG0@users.noreply.github.com>
2025-02-14 23:08:09 -0600

    Full Hamburger Menu Functionality

--- burger.js
+++ burger.js
@@ -1,7 +1,21 @@
 // mmmm... im hungy
-const hamburger = document.getElementsByClassName("hamburger")[0]
-const mobileNavs = document.getElementsByClassName("nav-links")[0]
+const hamburger = document.getElementById("hamburger");
+var mobileNavs = document.getElementById("nav-links");
+var hamburgerImage = document.getElementById("hamburger-img");
 
-hamburger.addEventListener("click", () => {
-  mobileNavs.classList.toggle("active")
-})
+var enabled = false;
+mobileNavs.style.display = 'none';
+
+hamburger.addEventListener("change", function(event){
+
+});
+
+hamburger.addEventListener("click", function(event){
+  if (enabled == false) {
+    enabled = true;
+    mobileNavs.style.display = 'block';
+  } else {
+    enabled = false;
+    mobileNavs.style.display = 'none';
+  }
+});
