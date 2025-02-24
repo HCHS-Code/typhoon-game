bdd62c5fceb2868ab2763faf0512855d05d4d97e
Jonathan Durrance <101822482+ANOG0@users.noreply.github.com>
2025-02-24 14:30:27 -0600

    Remove uneccessary code

--- script.js
+++ script.js
@@ -69,7 +69,6 @@ document.addEventListener("DOMContentLoaded", () => {
     const value = boardData[row][col];
 
     cell.textContent = value;
-    cell.id = value;
     cell.classList.add("revealed");
     cell.removeEventListener("click", handleCellClick); // Prevent re-clicking
 
